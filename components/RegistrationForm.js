73c3def013c2e298d09a5907d2db775b52ee066c
pacosta613 <pedro.acosta613@gmail.com>
2017-04-06 13:57:46 -0400

    Done

---
+++ components/RegistrationForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default class RegistrationForm extends React.Component {
+  render() {
+    <form>
+      <input type="text" />
+      <input type="password" />
+      <button type="submit">Submit</button>
+    </form>
+  }
+}
---
+++ components/Webpage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default class Webpage extends React.Component {
+  render() {
+    return (
+      <h1>The world's coolest webpage</h1>
+      <FillerText/>
+      <FillerText/>
+    )
+  }
+}
