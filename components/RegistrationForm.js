b971d8dcbd973c3c00db90940cf8b5f66c187340
ldgarber <leah.garber88@gmail.com>
2017-04-10 11:03:13 -0400

    first 2 tests passing

---
+++ components/RegistrationForm.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import PropTypes from 'prop-types';
+
+class RegistrationForm extends React.Component {
+  render(){
+    return (
+        <form>
+          <input type="text"></input>
+        </form>
+        )  
+  } 
+}
+
+export default RegistrationForm; 
