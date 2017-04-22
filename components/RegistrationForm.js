ac3d6aa8f9892c5d1f85ed31c4b735f24cae60da
David Kennell <davidkennell0@gmail.com>
2017-04-21 22:02:34 -0400

    Done.

--- components/RegistrationForm.js
+++ components/RegistrationForm.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-export class RegistrationForm extends React.Component {
-	render(){
-		return(
-			<form>
-			<input type={"text"}/>
-			<input type={"password"}/>
-			<button type={"submit"}/>
-			</form>
-			);
-	}
+export default class RegistrationForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <label>Username</label>
+        <input type="text" />
+        <label>Password</label>
+        <input type="password" />
+        <button type="submit">Register</button>
+      </form>
+    );
+  }
 }
--- components/Webpage.js
+++ components/Webpage.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import FillerText from "./components/FillerText"
+import React from 'react';
 
-export class Webpage extends React.Component {
-	render() {
-		return(
-			<div>
-			  <h1>The world's coolest webpage</h1>
-			  <FillerText/>
-			  <FillerText/>
-			</div>
-			)
-	}
+import FillerText from './FillerText';
+
+export default class Webpage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>The world's coolest webpage</h1>
+        <FillerText />
+        <FillerText />
+      </div>
+    );
+  }
 }
