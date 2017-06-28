f44f491135a725b66d9e6719c65e0051e55dec28
John Verdone <jmverdone@gmail.com>
2017-06-28 19:53:57 -0400

    Done.

---
+++ src/components/FillerText.js
@@ -0,0 +1,23 @@
+import React from 'react'
+class FillerText extends React.Component {
+  render(){
+    return (
+      <p>I am a filler text. I can be used to fill your screen. Amazing!</p>
+
+    )
+  }
+}
+
+export default FillerText
+// class Tweet extends React.Component {
+//   render() {
+//     return (
+//       <div className="tweet">
+//         <img src="http://twitter.com/some-avatar.png" className="tweet__avatar" />
+//         <div className="tweet__body">
+//             <p>We are writing this tweet in JSX. Holy moly!</p>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
