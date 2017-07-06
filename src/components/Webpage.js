50ed7d53783e53fbdba5755f8ee047756eec1208
mxdavis <md01@me.com>
2017-07-07 00:11:11 +0300

    Done.

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -5,9 +5,9 @@ import FillerText from './FillerText'
 class Webpage extends React.Component {
   render (){
     return (
-      React.createElement('title', {}, 'The world\'s coolest webpage'),
-      React.createElement(FillerText),
-      React.createElement(FillerText)
+      <title>The world's coolest webpage'</title>
+      <FillerText />
+      <FillerText />
     )
   }
 }
