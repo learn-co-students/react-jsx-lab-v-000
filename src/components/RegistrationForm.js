8a4bae9211576fcd87a7f36297966aa9ad7d39c2
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-08-06 08:57:18 -0400

    Done.

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class extends React.Component{
+  render(){
+    return (<form>
+        <input type="text" />
+        <input type="password" />
+        <button type='submit'></button>
+      </form>);
+  }
+}
---
+++ src/components/Webpage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FillerText from './FillerText.js'
+
+export default class extends React.Component{
+  render(){
+    return (<div>
+        <h1>The world's coolest webpage</h1>
+        <FillerText />
+        <FillerText />
+      </div>);
+  }
+}
