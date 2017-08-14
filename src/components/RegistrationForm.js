2571a31ceb40f69b8eeaa356794f0a9e69f58f36
github username <github email address>
2017-08-14 15:54:21 -0500

    initial commit

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class RegistrationForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" />
+          <input type="password" />
+          <button type="submit"/>
+        </form>
+      </div>
+    );
+  }
+}
---
+++ src/components/Webpage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FillerText from './FillerText';
+
+export class Webpage extends React.Component {
+  render() {
+    return (
+      <div>
+        <title>The worlds coolest webpage</title>
+        <FillerText />
+        <FillerText />
+      </div>
+    );
+  }
+}
