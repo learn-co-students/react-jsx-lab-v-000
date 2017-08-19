1a0d827531d7bbfb7ea632a220c4811ef11ee0f6
SiamKing <thesiameseelectric@gmail.com>
2017-08-18 20:36:53 -0700

    added label for form

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -4,9 +4,11 @@ class RegistrationForm extends React.Component {
   render() {
     return (
         <form>
+          <label>Username</label>
           <input type="text"/>
+          <label>Password</label>
           <input type="password"/>
-          <button type="submit"/>
+          <button type="submit">Register</button>
         </form>
     );
   }
