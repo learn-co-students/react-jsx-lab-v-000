30210a579ac959a6b4be0116d1aeb1e702f6fffd
Holger Paas <holger.paas@gmail.com>
2017-09-05 22:49:20 +0000

    Done.

---
+++ src/components/Webpage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FillerText from './FillerText';
+
+class Webpage extends React.Component {
+  render() {
+    return React.createElement('div',{},[
+      React.createElement('title',{},"The world's coolest webpage"),
+      React.createElement(FillerText),
+      React.createElement(FillerText)
+    ]);
+  }
+};
+
+export default Webpage;
