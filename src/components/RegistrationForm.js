d4200d7ed5214945fd2ce661a6160802c894f830
Motti <=>
2017-09-14 13:26:56 -0400

    Done.

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+ export default class RegistrationForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <label>Username</label>
+        <input type="text" />
+        <label>Password</label>
+        <input type="password" />
+        <button type="submit">Register</button>
+      </form>
+    );
+  }
+}
+
