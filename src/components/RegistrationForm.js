ef5d803049e67587ad89ce3d0fb640ef03d13bcc
Jennifer Pazos <jmp521@gmail.com>
2017-09-25 15:29:22 -0500

    Done.

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+class RegistrationForm extends Component {
+  render() {
+    return (
+
+        <form>
+          <input type="text" />
+          <input type="password" />
+          <button type="submit">Submit</button>
+        </form>
+    
+    )
+  }
+}
+
+export default RegistrationForm
---
+++ src/components/Webpage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import FillerText from './FillerText'
+
+class Webpage extends Component {
+  render() {
+    return (
+      <div>
+      The world's coolest webpage
+      <FillerText />
+      <FillerText />
+      </div>
+    )
+  }
+}
+
+export default Webpage
