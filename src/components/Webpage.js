0e14a3946aa248782350056180ccd52eb8f8923f
Bryan Hou <bryan.hou94@gmail.com>
2017-10-23 22:34:32 -0400

    Done.

---
+++ src/components/Webpage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import FillerText from './FillerText';
+
+export default class Webpages extends React.Component {
+	render(){
+		return (
+			<div>
+				<h1>The world's coolest webpage</h1>
+				<FillerText />
+				<FillerText />
+			</div>
+		);
+	}
+}
+
+
+
