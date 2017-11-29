e5ab3e89f9476cba5e19eb6f9ad5418a5d364a3c
laramontana <larabrightside@gmail.com>
2017-11-29 15:21:00 +0300

    Done.

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -1,12 +1,12 @@
+import FillerText from './FillerText';
 import React from 'react';
-import FillerText from './components/FillerText';
 
 class Webpage extends React.Component {
   render() {
     return (
       <div>
         <title>"The world's coolest webpage"</title>
-        React.createElement(FillerText)
+        React.createElement(FillerText) VS <FillerText /> ???
         React.createElement(FillerText)
       </div>
     )
