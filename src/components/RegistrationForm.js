e12c831f3fbf499c38cb8c174569361b76e4b61c
Sophia Hubeny <shhubeny@gmail.com>
2017-12-15 20:39:27 +0000

    Done.

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,13 @@
+class RegistrationForm extends React.Component{
+  render() {
+    return(
+      <form>
+        <input type="text" />
+        <input type="password" />
+        <button type="submit">
+      </form>
+    )
+  }
+}
+
+export default RegistrationForm;
---
+++ src/components/Webpage.js
@@ -0,0 +1,15 @@
+import FillerText from './components/FillerText';
+
+class Webpage extends React.Component{
+  render() {
+    return(
+      <div>
+        `The world's coolest webpage.`
+        <FillerText />
+        <FillerText />
+      </div>
+    )
+  }
+}
+
+export default Webpage;
