371531eb3c254b60795b982d59a8e7304e0561ab
Matt Hunger <matt.hunger@gmail.com>
2018-01-21 09:20:50 -0500

    Done.

--- src/components/FillerText.js
+++ src/components/FillerText.js
@@ -3,6 +3,10 @@ import React from 'react';
 
 export default class FillerText extends React.Component {
   render() {
-  return React.createElement('p', {}, "I am a filler text. I can be used to fill your screen. Amazing!")
+  return (
+    <p>
+      I am a filler text. I can be used to fill your screen. Amazing!
+    </p>
+    )
   }
 }
