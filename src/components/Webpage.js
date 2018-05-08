fe6cba73fa629894b993140ccb3bfbe88863b62e
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-05-08 15:05:50 +0000

    Done.

---
+++ src/components/Webpage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import FillerText from './FillerText'
+
+class Webpage extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>The world's largest webpage</h1>
+        <FillerText />
+        <FillerText />
+      </div>
+    )
+  }
+}
