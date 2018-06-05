d842ba00a0e4164e4883af65dcc689c9ef9ae364
jacobsilver2 <jacobsilver2@mac.com>
2018-06-05 18:51:46 -0400

    Done.

--- src/components/FillerText.js
+++ src/components/FillerText.js
@@ -7,9 +7,7 @@ import ReactDOM from 'react-dom'
 class FillerText extends React.Component {
     render(){
         return(
-            <div className="fillerText">
                 <p>I am a filler text. I can be used to fill your screen. Amazing!</p>
-            </div>
         )
     }
 }
--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -4,13 +4,11 @@ import ReactDOM from 'react-dom'
 class RegistrationForm extends React.Component {
     render(){
         return(
-            <div className="registrationForm">
                 <form>
                     <input type="text"/>
                     <input type="password"/>
                     <button type="submit"></button>
                 </form>
-            </div>
         )
     }
 }
