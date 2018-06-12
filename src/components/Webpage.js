698837ea720cd0cbe68580bf659218fa79efb2d6
kostanzhoglo <jeffreywithers@gmail.com>
2018-06-12 11:54:40 -0400

    Done.

---
+++ src/components/Webpage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import FillerText from './FillerText';
+
+export default class Webpage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          The world&#x27s coolest webpage
+        </h1>
+        <FillerText />
+        <FillerText />
+      </div>
+    )
+  }
+}
+
+// export default Webpage;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import RegistrationForm from './components/RegistrationForm';
+import FillerText from './components/FillerText';
 import Webpage from './components/Webpage';
 
 ReactDOM.render(
   (
     <div>
       <RegistrationForm />
+      <FillerText />
       <Webpage />
     </div>
   ),
