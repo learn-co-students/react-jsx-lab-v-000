0d0c140de14ae7f5622475d4eb18e4cd7c80478b
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-06-29 08:54:04 -0400

    Done.

---
+++ src/components/Webpage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import FillerText  from './FillerText'
+
+class Webpage extends React.Component {
+  render() {
+    return (
+      <div>
+        <text> The world's coolest webpage </text>
+        <FillerText />
+        <FillerText />
+        </div>
+    );
+  }
+}
+
+export default Webpage;
