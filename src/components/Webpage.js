9d6edb8c4b18cbea9f55a3a9425594847deb56db
github username <github email address>
2018-07-20 19:22:21 -0400

    Done.

---
+++ src/components/Webpage.js
@@ -0,0 +1,8 @@
+class Webpage extends React.Component{
+  render(){
+    return(
+      <title>Th world's coolest webpage</title>
+
+    )
+  }
+  export default Webpage
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 
 import RegistrationForm from './components/RegistrationForm';
 import Webpage from './components/Webpage';
+import FillerText from './components/FillerText';
 
 ReactDOM.render(
   (
     <div>
       <RegistrationForm />
       <Webpage />
+      <FillerText />
     </div>
   ),
   document.getElementById('global')
