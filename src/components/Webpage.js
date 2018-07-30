a98af5639c855fe20cce2d050254dba1bc641729
AndrewHongUW <32497926+AndrewHongUW@users.noreply.github.com>
2018-07-30 12:24:31 -0700

    Done.

---
+++ src/components/Webpage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import FillerText from './FillerText';
+
+export class Webpage extends React.Component {
+  render() {
+    return(
+      <div>
+        <title>The world's coolest webpage.</title>
+        < FillerText />
+        < FillerText />
+      </div>
+    )
+  }
+}
+
+export default Webpage;
