6977205f6f08fc371a524dc05b2853a22e422c82
James Randolph <james.randolph2016@gmail.com>
2018-08-22 23:02:26 +0000

    Done.

--- src/components/FillerText.js
+++ src/components/FillerText.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export default class FillerText extends React.Component {
-  render () {
+  render() {
     return (
-      <p>I am a filler text. I can be used to fill your screen. Amazing!</p>
-      )
+      <p>
+        I am a filler text. I can be used to fill your screen. Amazing!
+      </p>
+    )
   }
 }
--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -4,10 +4,10 @@ export default class RegistrationForm extends React.Component {
   render() {
     return (
       <form>
-      <input type="text" />
-      <input type="password" />
-      <button type="submit">Submit</button>
+        <input type="text" />
+        <input type="password" />
+        <button type="submit">Submit</button>
       </form>
-      )
+    )
   }
 }
