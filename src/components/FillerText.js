d7bf62d2c6ed1c496b65d33cfbf820a7b944c5f2
github username <github email address>
2018-09-10 14:13:00 -0400

    Done.

---
+++ src/components/FillerText.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class FillerText extends React.Component {
+  render() {
+    return (
+      <p>
+        I am a filler text. I can be used to fill your screen. Amazing!
+      </p>
+    );
+  };
+
+}
+
+
+export default FillerText;
