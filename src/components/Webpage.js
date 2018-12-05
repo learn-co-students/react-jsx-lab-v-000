5f91e3f1f471d1e02ac91cf3e40894223daacc69
terryblue99 <terryblue@comcast.net>
2018-12-05 16:00:19 -0600

    Done.

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'; // We need to import react so we can make use of its .component class
-import ReactDOM from 'react-dom'; // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
 import FillerText from './FillerText';
 
 export default class Webpage extends Component {
