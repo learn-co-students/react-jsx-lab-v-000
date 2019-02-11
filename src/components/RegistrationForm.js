ca2e2cba9693d9a3c466dd494b3dd0b2050d7341
Laura Correa <laura.correa@digitalunit.berlin>
2019-02-11 13:12:01 +0100

    Done.

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react' ;
+
+class RegistrationForm extends Component {
+    render (){
+        return(
+            <form>
+                <input type="text"></input>
+                <input type="password"></input>
+                <button type="submit">Submit</button> 
+            </form>
+        )
+    }
+}
+
+export default RegistrationForm
